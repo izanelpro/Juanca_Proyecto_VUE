16e8a3aad6a4d174c7ae969aabf941183c7dab55
Izan Ramirez <izanlage161005@gmail.com>
2024-12-04 10:44:47 +0100

    Nuevo_serve

---
+++ src/server.mjs
@@ -0,0 +1,24 @@
+// Zona importar
+import express from 'express';
+import http from 'http';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import rutas from './src/router/rutas.mjs';
+
+// Crear servidor
+const app=express();
+
+const server=http.createServer(app);
+
+//Puerto
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/api',rutas);
+app.set('port',process.env.PORT  || 5000);
+app.get('/',(req,res)=> {
+    res.send("Servidor en Marcha");
+})
+
+server.listen(app.get('port'),()=>{
+        console.log("Servidor corriendo en puerto ...",app.get('port'))
+})
