03dafab697334ce6729e6bbd9ed920d24ffb2c80
Izan Ramirez <izanlage161005@gmail.com>
2025-02-03 12:49:54 +0100

    TablaTienda creada y modificacion en CV de TablaEmpleo

--- src/components/TablaTienda.vue
+++ src/components/TablaTienda.vue
@@ -2,74 +2,11 @@
     <br>
 
     <div class="row">
-      <h5 class="font-weight-bold text-uppercase text-primary position-relative d-inline-block m-3">TRABAJA CON NOSOTROS<i class="bi bi-person-workspace m-2"></i></h5>
+      <h5 class="font-weight-bold text-uppercase text-primary position-relative d-inline-block m-3">TIENDA<i class="bi bi-person-workspace m-2"></i></h5>
       <router-link to="/" class="btn btn-customb"><i class="bi bi-arrow-return-left me-2"></i></router-link>
     </div>
 
     <br>
-    <div class="container-fluid border p-4">
-      <!-- Formulario -->
-      <form class="form-in-line">
-        <div class="col-10 col-m-6 col-lg-8 mx-auto">
-  
-          <!-- Nombres y Apellido -->
-          <div class="input-group-text mb-3">
-            <span class="input-group-text custom-span me-2">Apellidos: </span>
-            <input type="text" class="form-control sm w-50" placeholder="Apellidos" v-model="candidato.apellidos">
-            <span class="input-group-text custom-span ms-2 me-2">Nombre: </span>
-            <input type="text" class="form-control sm w-50" placeholder="Nombre" v-model="candidato.nombre">
-          </div>
-  
-          <!-- Email y Móvil -->
-          <div class="input-group-text mb-3">
-            <span class="input-group-text custom-span me-2">Email: </span>
-            <input type="email" class="form-control sm w-50" placeholder="Correo electrónico" v-model="candidato.email">
-            <span class="input-group-text custom-span ms-2 me-2">Móvil: </span>
-            <input type="text" class="form-control sm w-50" placeholder="Móvil" v-model="candidato.movil
-              " @blur="validarTelefono(this.candidato.movil)">
-          </div>
-
-          <!-- Categoría y Modalidad -->
-          <div class="input-group-text mb-3">
-            <span class="input-group-text custom-span me-2">Departamentos: </span>
-            <select name="departamento" class="form-control sm w-25" v-model="candidato.departamento">
-            <option value="">Departamentos</option>
-            <option v-for="departamento in departamentos" :key="departamento.id" :value="departamento">
-              {{ departamento.nm }}
-            </option>
-          </select>
-            <span class="input-group-text custom-span ms-auto me-2">Remoto: </span>  
-            <input class="mx-4" type="radio" name="modalidad" id="modalidad" value="remoto" v-model="candidato.modalidad">
-            <label for="remoto">Remoto</label>
-            <input class="mx-4" type="radio" name="modalidad" id="modalidad" value="hibrido" v-model="candidato.modalidad">
-            <label for="hibrido">Híbrido</label>
-            <input class="mx-4" type="radio" name="modalidad" id="modalidad" value="presencial" v-model="candidato.modalidad">
-            <label for="presencial">Presencial</label>
-          </div>
-          <!--Comentarios-->
-          <div class="input-group-text mb-3">
-            <span class="input-group-text custom-span me-2">Comentarios: </span>
-            <textarea name="comentarios" id="comentarios" cols="130" rows="5" v-model="candidato.comentarios"></textarea>
-          </div>
-          <!-- CV -->
-          <div class="input-group-text mb-3">
-            <span class="input-group-text custom-span me-2">CV(PDF): </span>
-            <input type="file" class="form-control sm w-50" @change="manejarSubidaDeArchivo" />
-          </div>
-          <!-- Aceptar politica  -->
-        <input type="checkbox" id="ckeckbox" v-model="candidato.avisoLegal">
-        <label for="checkbox"> Acepto las condiciones y términos de la <router-link to="/avisoLegal" >Política de Privacidad</router-link></label><br>
-          
-        </div>
-        
-        <!-- Submit -->
-        <button class="btn btn-primary m-1" @click.prevent="grabarcandidato">Alta</button>
-      </form>
-    </div>
-
-
-
-    
     <div class="container my-5" v-if="isAdmin">
       <h2 class="mb-4">Lista de Datos de Candidatos</h2>
       <div class="container my-2">
@@ -148,7 +85,6 @@
           movil: '',
           modalidad: '',
           avisoLegal: '',
-          cv: '',
           comentarios:''
         },
         candidatos: [],
@@ -175,15 +111,6 @@
   },
 
   methods: {
-    manejarSubidaDeArchivo(evento) {
-    const archivo = evento.target.files[0]; // Obtén el primer archivo
-      if (archivo) {
-        console.log('Archivo seleccionado:', archivo); // Verifica en consola el archivo
-        this.candidato.cv = archivo; // Asigna el archivo a la propiedad 'cv'
-      } else {
-        console.log('No se ha seleccionado ningún archivo');
-      }
-  },
     siguientePagina() {
       if (this.currentPage * this.pageSize < this.candidatos.length) {
         this.currentPage++;
--- src/server.mjs
+++ src/server.mjs
@@ -1,30 +1,108 @@
-// Zona importar
+// zona importaciones
 import express from 'express';
 import http from 'http';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import rutas from '../src/router/rutas.mjs';
+import path from 'path';
 import cors from 'cors';
+import multer from 'multer';
+import fs from 'fs';
 
-// Crear servidor
-const app=express();
-const server=http.createServer(app);
 
-//Puerto
-app.use(cors());
+
+// se crea el servidor
+const app = express();
+const server = http.createServer(app);
+
+
+// Habilitar CORS
+// Configurar CORS para permitir solicitudes del frontend
+app.use(cors({
+    origin: ['http://localhost:8080', 'http://localhost:5000'],  // Permitir el frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Permitir cookies y autenticación
+  }));
+
+// Middleware para manejar OPTIONS
+
+app.options('*', cors());  // Habilita CORS para preflight
+app.use(express.urlencoded({ extended: true })); // ¡IMPORTANTE!
 app.use(express.json());
-app.use(rutas);
 app.use(morgan('dev'));
+app.use(rutas);
 
 
-app.get('/',(req,res)=> {
-    res.send("Servidor en Marcha");
-})
-app.set('port',process.env.PORT  || 5000);
+//peticones
+app.get('/', (req,res) => {
+    res.send("Servidor para MongoDB");
+    });
 
-server.listen(app.get('port'),()=>{
-        console.log("Servidor corriendo en puerto ...",app.get('port'))
-})
-mongoose.connect("mongodb://root:abc123@localhost:27017/bbdd?authSource=admin")
-    .then(()=> console.log("Conectado a MongoDB"))
-    .catch(err=> console.log("Error conexión",err))
+//peticones
+app.get('/subircv', (req,res) => {
+  res.send("Paso por aquí");
+  });
+
+
+// Ruta para gestionar la subida de archivos
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = 'uploads/cv';
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+    cb(null, uploadDir);
+  },
+
+  filename: (req, file, cb) => {
+      // Usar el candidatoId enviado desde el frontend para nombrar el archivo
+      //const candidatoId = req.body.candidatoId || Date.now();  // Si no se envía candidatoId, usar la fecha actual como nombre
+      const fileExtension = path.extname(file.originalname);  // Obtener la extensión del archivo
+      const originalName = file.originalname.split('.')[0];  // Obtener el nombre original sin la extensión
+
+      // Concatenar candidatoId + el nombre original del archivo + la extensión
+      const filename = `${originalName}${fileExtension}`;  // Ejemplo: 1234567890-nombreOriginal.pdf
+  
+      cb(null, filename) // Guardar el archivo con el nombre generado
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ['application/pdf'];
+    if (!allowedTypes.includes(file.mimetype)) {
+      return cb(new Error('Tipo de archivo no permitido'), false);
+    }
+    cb(null, true);
+  }
+});
+
+// Ruta para gestionar la subida de archivos
+app.post('/subircv', upload.single('archivo'), (req, res) => {
+      console.log('Archivo recibido:', req.file);
+      console.log('Candidato ID:', req.body.candidatoId);
+      if (!req.file) {
+        return res.status(400).json({ mensaje: 'No se subió ningún archivo' });
+      }
+      // Responder con el archivo subido y su ubicación
+      res.status(200).json({
+        mensaje: 'Archivo subido con éxito',
+        archivo: req.file,
+      });
+    });
+//elegimos puerto
+
+app.set('port', process.env.PORT  || 5000);
+
+server.listen(app.get('port'),() => {
+    console.log("servidor corriendo en puerto ...", app.get('port'))
+});
+
+mongoose.connect('mongodb://admin:abc123@localhost:27017/bbdd?authSource=admin')
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.log("Error Conexion",err)) 
+
+
+
